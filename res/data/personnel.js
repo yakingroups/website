490fef2d4fbc5a6f0fd7231ebdb84cebdb595cd0
cvmurbaut <cvmurbaut@protonmail.com>
2021-11-08 16:35:38 +0700

    init

---
+++ res/data/personnel.js
@@ -0,0 +1,146 @@
+personnels = {
+    0: {
+        "name": "Achmad Torikul Huda",
+        "image": "res/images/personnels/torik.jpeg",
+        "spacializations": ["Web Programming", "Microcontroller", "OpenCV"],
+        "experiences":[
+            {
+                "type": "Internship",
+                "section": "Pendidikan Ahli Pemrograman Sistem Informasi",
+                "location": "PAPSI-ITS",
+                "year": 2017
+            },
+            {
+                "type": "Internship",
+                "section": "Web Development",
+                "location": "-",
+                "year": 2020
+            },
+            {
+                "type": "Internship",
+                "section": "Project Development",
+                "location": "PT.Sinergi Informatika Semen Indonesia",
+                "year": 2021
+            }
+        ],
+        "links":{
+            "github": "https://github.com/achmadthorik"
+        },
+        "contact": {
+            "email": "achmadthorik2@gmail.com"
+        },
+        "certificates": [
+            "res/images/certificates/torik/2.jpg", "res/images/certificates/torik/3.png", "res/images/certificates/torik/1.png", "res/images/certificates/torik/5.png", "res/images/certificates/torik/4.png", "res/images/certificates/torik/6.png"
+        ]
+    },
+    1:{
+        "name": "Dany Razzaq Farino",
+        "image": "res/images/personnels/drf.jpg",
+        "spacializations": ["Web Programming", "Desktop Programming", "Python", "Tensorflow"],
+        "experiences":[
+            {
+                "type": "Internship",
+                "section": "Web Development",
+                "location": "PT. Jawa Pos Group IT",
+                "year": 2017
+            },
+            {
+                "type": "Internship",
+                "section": "Internal Web Development",
+                "location": "PT. Jawa Pos Group Media",
+                "year": 2021
+            }
+        ],
+        "links":{
+            "github": "https://github.com/admiralpuni",
+            "portofolio": "https://admiralpuni.github.io/portofolio/"
+        },
+        "contact": {
+            "email": "drfredmagic@gmail.com"
+        },
+        "certificates": [
+            "res/images/certificates/drf/1.png", "res/images/certificates/drf/2.png", "res/images/certificates/drf/3.png", "res/images/certificates/drf/4.JPG"
+        ]
+    },
+    2:{
+        "name": "Ahmad Nasrudin",
+        "image": "res/images/personnels/an.jpeg",
+        "spacializations": ["PLC Systems", "Mechanical Systems", "Graphic Design"],
+        "experiences":[
+            {
+                "type": "Internship",
+                "section": "IT Division",
+                "location": "PT. Sari Warna Asli Garment, Surakarta",
+                "year": 2017
+            },
+            {
+                "type": "Internship",
+                "section": "Robotics",
+                "location": "Robo Edu Maland",
+                "year": 2020
+            }
+        ],
+        "links":{},
+        "contact": {
+            "email": "ahmadnasrudin0723@gmail.com",
+            "phone": "089696979863"
+        },
+        "certificates": [
+            "res/images/certificates/drf/1.png"
+        ]
+    },
+    3:{
+        "name": "Bagas Kurniawan",
+        "image": "res/images/personnels/bk.jpeg",
+        "spacializations": ["Electrical Installation", "Electrical Control", "Electrical Inspection", "Electrical Instrument"],
+        "experiences":[
+            {
+                "type": "Internship",
+                "section": "Electrical Instalation and equipment",
+                "location": "PT Madukismo",
+                "year": 2017
+            },
+            {
+                "type": "Internship",
+                "section": "Panel Maker and Programmer PLC control",
+                "location": "PT Laksana Daya Prima Sidoarjo",
+                "year": 2021
+            }
+        ],
+        "links":{},
+        "contact": {
+            "email": "bagaskurniawan075@gmail.com",
+            "phone": "0895396651151"
+        },
+        "certificates": [
+            "res/images/certificates/bk/1.jpeg","res/images/certificates/bk/2.jpeg","res/images/certificates/bk/3.jpeg"
+        ]
+    }
+    ,
+    4:{
+        "name": "Prishandy Hamani Amrulloh",
+        "image": "res/images/personnels/pma.jpeg",
+        "spacializations": ["Electrical Systems", "Microcontroller", "Robotics", "IoT Systems"],
+        "experiences":[
+            {
+                "type": "Competition",
+                "section": "Industrial Control",
+                "location": "LKS Jawa Timur",
+                "year": 2017
+            },
+            {
+                "type": "Competition",
+                "section": "Robotics",
+                "location": "Lomba Robot Indonesia Tingkat Nasional",
+                "year": 2021
+            }
+        ],
+        "links":{},
+        "contact": {
+            "email": "prishandyhamami@gmail.com"
+        },
+        "certificates": [
+            "res/images/certificates/pma/fpcPA-1103181031-Prishandy Hamami Amrulloh - 031_Prishandy Hamami.jpg", "res/images/certificates/pma/Sertifikat Peserta - 031_Prishandy Hamami.jpg", "res/images/certificates/pma/Sertifikat_UKK_Belakang - 031_Prishandy Hamami.jpg", "res/images/certificates/pma/IMG_0803 - 031_Prishandy Hamami.JPG", "res/images/certificates/pma/LKS Kabupaten - 031_Prishandy Hamami.jpg", "res/images/certificates/pma/Prishandy Hamami Amrulloh (juara 2 regional) - 031_Prishandy Hamami.jpg", "res/images/certificates/pma/Prishandy Hamami Amrulloh (juara 1 nasional) - 031_Prishandy Hamami.jpg", "res/images/certificates/pma/Prishandy Hamami Amrulloh (design terbaik regional) - 031_Prishandy Hamami.jpg", "res/images/certificates/pma/Sertifikat_UKK - 031_Prishandy Hamami.jpg", "res/images/certificates/pma/Prishandy Hamami Amrulloh (design terbaik nasional) - 031_Prishandy Hamami.jpg"
+        ]
+    }
+}
